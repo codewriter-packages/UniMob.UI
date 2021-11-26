606a0da5c4b020c96b3f0e7bfcb4f20eef753e8a
VladV <vanifatov.vlad@yandex.ru>
2021-11-26 13:18:18 +0300

    Add Bind extension

--- Runtime/ClickExtensions.cs
+++ Runtime/ClickExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using JetBrains.Annotations;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace UniMob.UI
@@ -9,28 +10,81 @@ namespace UniMob.UI
         public static void Click([NotNull] this Button button, [NotNull] Func<Action> call)
         {
             if (button == null) throw new ArgumentNullException(nameof(button));
-            if (call == null) throw new ArgumentNullException(nameof(call));
 
-            void Listener()
-            {
-                var handler = call.Invoke();
-                handler?.Invoke();
-            }
-
-            button.onClick.AddListener(Listener);
+            Bind(button.onClick, call);
         }
 
         public static void Click([NotNull] this Button button, [NotNull] Action call)
         {
             if (button == null) throw new ArgumentNullException(nameof(button));
+
+            Bind(button.onClick, call);
+        }
+
+        public static void Bind([NotNull] this UnityEvent unityEvent, [NotNull] Func<Action> call)
+        {
+            if (unityEvent == null) throw new ArgumentNullException(nameof(unityEvent));
             if (call == null) throw new ArgumentNullException(nameof(call));
 
             void Listener()
             {
-                call.Invoke();
+                using (Atom.NoWatch)
+                {
+                    var handler = call.Invoke();
+                    handler?.Invoke();
+                }
             }
 
-            button.onClick.AddListener(Listener);
+            unityEvent.AddListener(Listener);
+        }
+
+        public static void Bind([NotNull] this UnityEvent unityEvent, [NotNull] Action call)
+        {
+            if (unityEvent == null) throw new ArgumentNullException(nameof(unityEvent));
+            if (call == null) throw new ArgumentNullException(nameof(call));
+
+            void Listener()
+            {
+                using (Atom.NoWatch)
+                {
+                    call.Invoke();
+                }
+            }
+
+            unityEvent.AddListener(Listener);
+        }
+
+        public static void Bind<T>([NotNull] this UnityEvent<T> unityEvent, [NotNull] Func<Action<T>> call)
+        {
+            if (unityEvent == null) throw new ArgumentNullException(nameof(unityEvent));
+            if (call == null) throw new ArgumentNullException(nameof(call));
+
+            void Listener(T value)
+            {
+                using (Atom.NoWatch)
+                {
+                    var handler = call.Invoke();
+                    handler?.Invoke(value);
+                }
+            }
+
+            unityEvent.AddListener(Listener);
+        }
+
+        public static void Bind<T>([NotNull] this UnityEvent<T> unityEvent, [NotNull] Action<T> call)
+        {
+            if (unityEvent == null) throw new ArgumentNullException(nameof(unityEvent));
+            if (call == null) throw new ArgumentNullException(nameof(call));
+
+            void Listener(T value)
+            {
+                using (Atom.NoWatch)
+                {
+                    call.Invoke(value);
+                }
+            }
+
+            unityEvent.AddListener(Listener);
         }
     }
 }
