133ee168291a4e76c442e93adb1492e644ba4bfb
VladV <vanifatov.vlad@yandex.ru>
2022-09-16 15:01:30 +0300

    Back button handling

---
+++ Runtime/Widgets/BackButtonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace UniMob.UI.Widgets
+{
+    public class BackButtonController
+    {
+        private Func<bool> _handler;
+
+        public void RegisterHandler([NotNull] Func<bool> handler)
+        {
+            _handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+
+        public void RegisterHandler([NotNull] NavigatorState handler)
+        {
+            if (handler == null)
+            {
+                throw new ArgumentNullException(nameof(handler));
+            }
+
+            _handler = handler.HandleBack;
+        }
+
+        public bool HandleBack()
+        {
+            return _handler?.Invoke() ?? true;
+        }
+
+        public static TRoute Create<TRoute>([NotNull] Func<BackButtonController, TRoute> func)
+            where TRoute : Route
+        {
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            var backButtonController = new BackButtonController();
+            var route = func.Invoke(backButtonController);
+            route.BackAction = backButtonController.HandleBack;
+            return route;
+        }
+    }
+}
