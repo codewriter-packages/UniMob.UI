8078df4c469af2e66cc3fa60a26ccbac72e716c2
VladV <vanifatov.vlad@yandex.ru>
2022-12-25 11:14:32 +0400

    Replace Text with TextMeshPro in UniMobText widget

--- Runtime/Widgets/UniMobText.cs
+++ Runtime/Widgets/UniMobText.cs
@@ -4,17 +4,21 @@ namespace UniMob.UI.Widgets
 {
     public class UniMobText : StatefulWidget
     {
+        public UniMobText()
+        {
+        }
+
         public UniMobText(WidgetSize size)
         {
             Size = size;
         }
 
-        public WidgetSize Size { get; }
-
+        public WidgetSize? Size { get; set; }
         public string Value { get; set; }
         public Color Color { get; set; } = Color.black;
         public CrossAxisAlignment CrossAxisAlignment { get; set; } = CrossAxisAlignment.Start;
         public MainAxisAlignment MainAxisAlignment { get; set; } = MainAxisAlignment.Start;
+        public float MaxCrossAxisExtent { get; set; } = int.MaxValue;
 
         public int FontSize { get; set; } = 12;
 
@@ -33,6 +37,22 @@ namespace UniMob.UI.Widgets
         public MainAxisAlignment MainAxisAlignment => Widget.MainAxisAlignment;
         public CrossAxisAlignment CrossAxisAlignment => Widget.CrossAxisAlignment;
 
-        public override WidgetSize CalculateSize() => Widget.Size;
+        public override WidgetSize CalculateSize()
+        {
+            if (Widget.Size.HasValue)
+            {
+                return Widget.Size.Value;
+            }
+
+            var prefab = UniMobViewContext.Loader.LoadViewPrefab(View);
+            var tmpText = prefab.gameObject.GetComponent<UniMobTextMeshProBehaviour>();
+
+            tmpText.fontSize = FontSize;
+
+            var maxExtent = Widget.MaxCrossAxisExtent;
+            var size = tmpText.GetPreferredValues(Value, maxExtent, int.MaxValue);
+            size.x = Mathf.Min(maxExtent, size.x);
+            return WidgetSize.Fixed(size.x, size.y);
+        }
     }
 }
