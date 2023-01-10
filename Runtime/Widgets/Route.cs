3905415246fc7282695c30db34039f15299f9e73
VladV <vanifatov.vlad@yandex.ru>
2023-01-10 12:39:35 +0400

    New Navigator API

--- Runtime/Widgets/Route.cs
+++ Runtime/Widgets/Route.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UniMob.UI.Internal;
 
 namespace UniMob.UI.Widgets
@@ -11,6 +12,7 @@ namespace UniMob.UI.Widgets
         private readonly RouteSettings _settings;
         private readonly TriggerStateMachine<ScreenState, ScreenEvent, Task> _machine;
         private readonly TaskCompletionSource<object> _popCompleter = new TaskCompletionSource<object>();
+        private readonly TaskCompletionSource<object> _pushCompleter = new TaskCompletionSource<object>();
 
         private Func<bool> _backAction;
         private object _popResult = null;
@@ -26,6 +28,8 @@ namespace UniMob.UI.Widgets
         public RouteModalType ModalType => _settings.ModalType;
 
         public Task<object> PopTask => _popCompleter.Task;
+
+        public Task PushTask => _pushCompleter.Task;
 
         public string Key => _settings.Name;
 
@@ -110,6 +114,7 @@ namespace UniMob.UI.Widgets
 
         public Task Initialize()
         {
+            _pushCompleter.SetResult(null);
             return OnInitialize() ?? Task.CompletedTask;
         }
 
@@ -131,7 +136,7 @@ namespace UniMob.UI.Widgets
 
         protected virtual Task OnDestroy()
         {
-            _popCompleter.SetResult(this._popResult);
+            _popCompleter.SetResult(_popResult);
             return Task.CompletedTask;
         }
 
@@ -147,6 +152,12 @@ namespace UniMob.UI.Widgets
         void IBackActionOwner.SetBackAction(Func<bool> action)
         {
             _backAction = action;
+        }
+
+        [Obsolete("await route is Obsolete. Use route.PopTask or route.PushTask instead")]
+        public TaskAwaiter<object> GetAwaiter()
+        {
+            return PopTask.GetAwaiter();
         }
     }
 
