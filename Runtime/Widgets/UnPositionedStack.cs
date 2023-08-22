0dc9fafe3336615c521f8c70cd00f70a67645cdf
VladV <vanifatov.vlad@yandex.ru>
2023-08-22 11:52:37 +0400

    Add UnPositionedStack widget

---
+++ Runtime/Widgets/UnPositionedStack.cs
@@ -0,0 +1,18 @@
+﻿namespace UniMob.UI.Widgets
+{
+    public class UnPositionedStack : MultiChildLayoutWidget
+    {
+        public override State CreateState() => new UnPositionedStackState();
+    }
+
+    internal class UnPositionedStackState : MultiChildLayoutState<UnPositionedStack>, IUnPositionedStackState
+    {
+        public override WidgetViewReference View { get; }
+            = WidgetViewReference.Resource("$$_UnPositionedStack");
+
+        public override WidgetSize CalculateSize()
+        {
+            return WidgetSize.Stretched;
+        }
+    }
+}
