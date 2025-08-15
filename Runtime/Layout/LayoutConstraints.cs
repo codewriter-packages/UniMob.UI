24e9e0911427c270c56ccede9b6dd273fbf00745
Giovanni Moscato <giovi94@gmail.com>
2025-08-15 18:09:18 +0200

    feat: Define primitives to support Layout-aware widgets
    This commit defines all the basic structures and classes to make the constraints aware sizing protocol work within UniMob

---
+++ Runtime/Layout/LayoutConstraints.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace UniMob.UI.Layout
+{
+    public readonly struct LayoutConstraints
+    {
+        public float MinWidth { get; }
+        public float MinHeight { get; }
+        public float MaxWidth { get; }
+        public float MaxHeight { get; }
+
+        public LayoutConstraints(float minWidth, float minHeight, float maxWidth, float maxHeight)
+        {
+            MinWidth = minWidth;
+            MinHeight = minHeight;
+            MaxWidth = maxWidth;
+            MaxHeight = maxHeight;
+        }
+
+        /// <summary>
+        /// Creates a set of tight constraints, forcing an exact size.
+        /// </summary>
+        public static LayoutConstraints Tight(float width, float height)
+        {
+            return new LayoutConstraints(width, height, width, height);
+        }
+        
+
+        public override string ToString()
+        {
+            return $"Constraints(w:[{MinWidth}-{MaxWidth}], h:[{MinHeight}-{MaxHeight}])";
+        }
+    }
+}
---
+++ Runtime/Layout/LayoutState.cs
@@ -0,0 +1,51 @@
+﻿using UniMob.UI.Layout.Internal.RenderObjects;
+
+namespace UniMob.UI.Layout
+{
+    // Interface for easier type-checking later without generics.
+    public interface ILayoutState : IViewState
+    {
+        RenderObject RenderObject { get; }
+    }
+
+    /// <summary>
+    /// The base State for a LayoutWidget. It creates and owns the RenderObject.
+    /// </summary>
+    public abstract class LayoutState<TWidget> : ViewState<TWidget>, ILayoutState where TWidget : LayoutWidget, Widget
+    {
+        private RenderObject _renderObject;
+
+        public RenderObject RenderObject
+        {
+            get
+            {
+                if (_renderObject == null)
+                {
+                    _renderObject = Widget.CreateRenderObject(Context, this);
+                }
+                return _renderObject;
+            }
+        }
+
+        internal sealed override void Update(Widget widget)
+        {
+            base.Update(widget);
+            // If the widget is updated, we need to reset the RenderObject.
+            this._renderObject = null;
+        }
+
+        // This method provides the bridge TO the old layout system.
+        // When a legacy widget contains a new layout-aware widget, this is called.
+        public override WidgetSize CalculateSize()
+        {
+            var ro = RenderObject;
+            
+            // For legacy parents, report the unconstrained intrinsic size.
+            // This is a "best guess" since no constraints are provided.
+            var intrinsicWidth = ro.GetIntrinsicWidth(float.PositiveInfinity);
+            var intrinsicHeight = ro.GetIntrinsicHeight(intrinsicWidth);
+
+            return WidgetSize.Fixed(intrinsicWidth, intrinsicHeight);
+        }
+    }
+}
