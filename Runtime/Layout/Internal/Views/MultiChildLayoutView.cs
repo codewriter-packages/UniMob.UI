66ac3804c74dadae2bfe0e6bdf31ff462fee61cb
Giovanni Moscato <giovi94@gmail.com>
2025-08-20 16:16:28 +0200

    fix: make an error message more expressive

--- Runtime/Layout/Internal/Views/MultiChildLayoutView.cs
+++ Runtime/Layout/Internal/Views/MultiChildLayoutView.cs
@@ -44,8 +44,8 @@ namespace UniMob.UI.Layout.Internal.Views
 
                 if (float.IsInfinity(layoutData.Size.x) || float.IsInfinity(layoutData.Size.y))
                     throw new InvalidOperationException(
-                        $"Child {child.GetType().Name} at position {i} has an unbounded size." +
-                        $"This is not supported in MultiChildLayoutView." +
+                        $"Child {child.GetType().Name} at position {i} has an unbounded size. " +
+                        $"This is not supported in MultiChildLayoutView. " +
                         $"Try wrapping it in a Container or similar widget to constrain its size.");
 
 
