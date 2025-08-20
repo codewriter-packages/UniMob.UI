17946bd1ddd828f1e3ab73a04dbd060ffb684a83
Giovanni Moscato <giovi94@gmail.com>
2025-08-20 12:05:30 +0200

    feat: more descriptiive error message in MultiChildLayoutView

--- Runtime/Layout/Internal/Views/MultiChildLayoutView.cs
+++ Runtime/Layout/Internal/Views/MultiChildLayoutView.cs
@@ -44,7 +44,9 @@ namespace UniMob.UI.Layout.Internal.Views
 
                 if (float.IsInfinity(layoutData.Size.x) || float.IsInfinity(layoutData.Size.y))
                     throw new InvalidOperationException(
-                        $"Child {i} has an unbounded size. This is not supported in MultiChildLayoutView.");
+                        $"Child {child.GetType().Name} at position {i} has an unbounded size." +
+                        $"This is not supported in MultiChildLayoutView." +
+                        $"Try wrapping it in a Container or similar widget to constrain its size.");
 
 
                 var childView = render.RenderItem(child);
